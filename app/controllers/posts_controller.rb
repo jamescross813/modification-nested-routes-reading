674635aa1f1b3c4569dd8f475d211c3f56a769fe
jamescross <crossjames813@gmail.com>
2021-07-03 13:32:21 -0500

    commit tree 9408d57a32fe2aab2c2e77af0225c952dd122b8b

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,8 +17,8 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
-  end
+    @post = Post.new(author_id: params[:author_id])
+  end 
 
   def create
     @post = Post.new(post_params)
@@ -38,7 +38,7 @@ class PostsController < ApplicationController
 
   private
 
-  def post_params
-    params.require(:post).permit(:title, :description)
-  end
+def post_params
+  params.require(:post).permit(:title, :description, :author_id)
+end 
 end
